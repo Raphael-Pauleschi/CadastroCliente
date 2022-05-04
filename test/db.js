0b30060ba3a1da7b97a6344263802e5a51d3c80d
Raphael-Pauleschi <Raphael.f.pauleschi@gmail.com>
2022-05-04 09:27:23 -0300

    Database

--- test/db.js
+++ test/db.js
@@ -22,4 +22,17 @@ async function insertUsuario(){
     return await conn.query(sql,values);
 }
 
-module.exports = {selectUsuario, insertUsuario}
+async function deleteUsuario(id){
+    const conn =await connect();
+    const sql = 'DELETE FROM usuario where id=?;';
+    return await conn.query(sql,[id]);
+}
+
+async function updateUsuario(id, usuario){
+    const conn = await connect();
+    const sql = 'UPDATE usuario SET nome=?, senha=? WHERE id=?';
+    const values = [usuario.nome, usuario.senha, id];
+    return await conn.query(sql,values);
+}
+
+module.exports = {selectUsuario, insertUsuario, deleteUsuario, updateUsuario}
