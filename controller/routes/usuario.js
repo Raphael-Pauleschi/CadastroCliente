d51a823f557f7911667c655a472a78cd77f2cce9
Raphael Pauleschi <84625141+Raphael-Pauleschi@users.noreply.github.com>
2022-05-11 11:13:22 -0300

    Add files via upload

--- controller/routes/usuario.js
+++ controller/routes/usuario.js
@@ -7,7 +7,7 @@ module.exports = function(app){
             res.render('usuario/Cadastro', {mensagem: 'Cadastro'});
         else
             res.render('usuario/Cadastro', {mensagem: null});
-    })
+    });
 
     app.post('/cadastro/usuario/edit/salvar', (req,res)=>{
         var usuario = {nome: req.body.nome,senha: req.body.senha,id: req.body.id};
@@ -19,5 +19,36 @@ module.exports = function(app){
         }
 
 
-    })
+    });
+    app.post('/cadastro/usuario/salvar',seguranca.autenticar,(req,res)=> {
+        try{
+            var usuario = {nome: req.body.nome,
+            senha:seguranca.ocultarsenha(req.body.senha)}
+            usuarioBanco.insertUsuario(usuario);
+            res.render('usuario/Sucesso', {mensagem: 'cadastrado'});
+        }catch(error){
+            console.log(error);
+            res.render('usuario/Cadastro', {title: 'Cadastro',
+        mensagem: 'Erro no cadastrado'});
+        }
+    });
+
+    app.get('/lista/usuario',segurnca.autenticar, async(req,res,next)=>{
+        try{
+            const docs = await usuarioBanco.selectUsuario();
+            res.render('usuario/Lista', {mensagem: 'Lista de Usuários', docs});
+        }catch(err){
+            next(err);
+        }
+    });
+    app.get('delete/usuario/id',seguranca.autenticar, async(req,res,next)=>{
+        try{
+            var id = req.params.id;
+            await usuarioBanco.deleteUsuario(id);
+            const docs = await usuarioBanco.selectUsuario();
+            res.render('usuario/Lista', {mensagem: 'Usuário excluído com sucesso',docs});
+        }catch(err){
+            next(err);
+        }
+    });
 }
